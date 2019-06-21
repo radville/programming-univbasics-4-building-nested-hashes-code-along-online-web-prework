2cbcd572cb7dee3dc1b97ee078beba347325ad8b
Laura Radville <laura.radville@gmail.com>
2019-06-21 12:58:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,12 +22,12 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []
